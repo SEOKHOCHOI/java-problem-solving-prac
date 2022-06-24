ce484c944478244652c4b1465c169d3695f5fe26
SEOKHO <seokho9473@gmail.com>
2022-06-25 06:46:59 +0900

    Learning class concept

--- src/problem1/Person1.java
+++ src/problem1/Person1.java
@@ -1,4 +1,13 @@
 package problem1;
 
+// 클래스는 결국 하나의 "타입"이다. 마치 int, double 등 처럼.
+// 다만 int, double 처럼 Java가 미리 정해놓은 타입이 아니라 사용자가 정의한 새로운 타입이라
+// 는 의미에서 "사용자 정의 타입"이라고 부르기도 한다.
+// int 혹은 double 형 변수를 선언하고 사용하는 것처럼 Person1형 변수를 선언하고 사용한다.
+// int count = 0,    Person1 first = new Person1();
 public class Person1 {
+    public String name;  // 클래스의 field or data member라 부름
+    public String number;
 }
+
+
---
+++ src/problem1/code01.java
@@ -0,0 +1,34 @@
+package problem1;
+
+public class code01 {
+
+    public static void main(String[] agrs) {
+
+        // 1. 변수를 만들고
+         Person1 first;
+
+        // 2. new는 항상 뭔가를 생성하는 일을 함.
+        // 이때 new가 생성하는걸 객체라고 부름.
+         first = new Person1();
+
+         // 클래스 안의 필드들을 엑세스 하기위해 . 을 찍어줌.
+         // 여기까진 c의 구조체와 완전히 동일.
+         first.name = "John";
+         first.number = "01012345678";
+
+         System.out.println("name: " + first.name + ", Number: " + first.number);
+
+         // Person1 이라는게 하나의 데이터 타입이기에
+         // int나 double같은 기본 타입으로 가능한 것들을 다 할 수 있음.
+         // 그러니 배열도 선언가능.
+         Person1 [] members = new Person1 [2];
+         members[0] = first;
+         // 두번째 칸에 새로운 사람을 저장하고 싶으면 new personw1() 으로 객체를 만들고
+         // 이 객체의 name필드에 추가 해주면 됨.
+         members[1] = new Person1();
+         members[1].name = "David";
+         members[1].number = "234234234";
+         System.out.println(members[1]);
+         System.out.println("memebers[1].name: " + members[1].name + ", members[1].number: " + members[1].number);
+    }
+}
